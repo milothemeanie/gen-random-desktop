326719eca170edd5172cc325f9e2c178202dc5ea
cward <milothemeanie@gmail.com>
2019-03-18 22:55:47 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,26 @@ struct Photo {
     height: u32,
 }
 
+const CLIENT_ID : &'static str = "3ac00ee4846a33d1d4b87cdff1d57f7471309a7d5b2639cba011a2187eee4cad";
+
 fn main() {
     println!("retrieving a random photo");
-    let response = requests::get("https://api.unsplash.com/photos/random?client_id=3ac00ee4846a33d1d4b87cdff1d57f7471309a7d5b2639cba011a2187eee4cad").unwrap();
-    let data = response.json().unwrap();
+    let data = retrieve_photo();
 
-//    print!("{}", data["id"]);
+    println!("received photo");
+    println!("id:{} description:{}, downloadlink:{}, width:{}, height: {}",
+             data.id, data.description, data.download_link, data.width, data.height);
+
+
+    let path_string = &format!("/tmp/{}.jpg", data.id);
+    write_image(data, path_string);
+
+    set_wallpaper_cinnamon(path_string);
+}
+
+fn retrieve_photo() -> Photo {
+    let response = requests::get(format!("https://api.unsplash.com/photos/random?client_id={}", CLIENT_ID)).unwrap();
+    let data = response.json().unwrap();
     let data = Photo {
         id: data["id"].to_string(),
         description: data["description"].to_string(),
@@ -29,22 +43,11 @@ fn main() {
         width: data["width"].as_u32().unwrap(),
         height: data["height"].as_u32().unwrap(),
     };
+    data
+}
 
-    println!("received photo");
-    println!("id:{} description:{}, downloadlink:{}, width:{}, height: {}",
-             data.id, data.description, data.download_link, data.width, data.height);
-
-    let image_response = requests::get(data.download_link).unwrap();
-    let image_response = image_response.content();
-
-    let path_string = &format!("/tmp/{}.jpg", data.id);
-    let path = Path::new(path_string);
-
-    fs::write(path, image_response).expect("failed to write image");
-
+fn set_wallpaper_cinnamon(path_string: &String) {
     let image_parm = format!(r#"'file:///{}'"#, path_string);
-
-
     Command::new("dconf")
         .arg("write")
         .arg("/org/cinnamon/desktop/background/picture-uri")
@@ -53,5 +56,10 @@ fn main() {
         .expect("failed to set the wallpaper");
 }
 
-//https://api.unsplash.com/photos/random?client_id=3ac00ee4846a33d1d4b87cdff1d57f7471309a7d5b2639cba011a2187eee4cad
-//gconftool-2 -t str --set /desktop/gnome/background/picture_filename
+fn write_image(data: Photo, path_string: &String) {
+    let path = Path::new(path_string);
+    let image_response = requests::get(data.download_link).unwrap();
+    let image_response = image_response.content();
+    fs::write(path, image_response).expect("failed to write image");
+}
+
